c8dce9834188e90b282f36dbeb3c54b3b9554758
frizzon00 <alexandre.frizon@sptech.school>
2024-05-14 21:25:09 -0300

    Tela de Jogos e Quiz

---
+++ Site/forcaJogo/scripts/word-list.js
@@ -0,0 +1,43 @@
+const wordList = [
+    {
+        word: "jrasil",
+        hint: "O maior país da América do Sul, conhecido por sua Floresta Amazônica e pelo Carnaval."
+      },
+      {
+        word: "japão",
+        hint: "Uma nação insular na Ásia Oriental, conhecida por seu rico patrimônio cultural e tecnologia avançada."
+      },
+      {
+        word: "itália",
+        hint: "Um país europeu famoso por sua deliciosa culinária, arte e arquitetura."
+      },
+      {
+        word: "austrália",
+        hint: "Um continente e país conhecido por sua fauna única, incluindo cangurus e coalas."
+      },
+      {
+        word: "frança",
+        hint: "Um país na Europa conhecido por sua cultura, culinária e monumentos icônicos como a Torre Eiffel."
+      },
+      {
+        word: "canadá",
+        hint: "Um país na América do Norte famoso por suas paisagens deslumbrantes, como as Montanhas Rochosas e as Cataratas do Niágara."
+      },
+      {
+        word: "méxico",
+        hint: "Um país na América do Norte conhecido por sua rica herança cultural, comida saborosa e praias deslumbrantes."
+      },
+      {
+        word: "india",
+        hint: "Um país no sul da Ásia conhecido por sua diversidade cultural, monumentos históricos e culinária picante."
+      },
+      {
+        word: "argentina",
+        hint: "Um país na América do Sul famoso por seu tango, carne suculenta e paisagens impressionantes, como as Cataratas do Iguaçu."
+      },
+      {
+        word: "espanha",
+        hint: "Um país na Europa conhecido por suas praias ensolaradas, arquitetura deslumbrante e deliciosas tapas."
+      }
+  ];
+  
